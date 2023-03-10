dadd1d0f9946977f341b0baa835ed8a7e0d87ea1
Sevak Grigorian <sevak.grigoryan2021@gmail.com>
2023-03-10 21:10:41 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import AuthPage from './AuthPage';
+import ChatsPage from './ChatsPage';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(undefined);
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
